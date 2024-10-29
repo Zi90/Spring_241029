0ad37ae7a864f095823dd0dd0b21894c5f592922
Zi90 <edwin0322@gmail.com>
2024-10-29 18:17:03 +0900

    day02

---
+++ src/main/java/com/ezen/spring/domain/BoardVO.java
@@ -0,0 +1,31 @@
+package com.ezen.spring.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardVO {
+//	bno bigint auto_increment,
+//	title varchar(500) not null,
+//	writer varchar(500) not null,
+//	content text,
+//	is_del varchar(5) default 'N',
+//	reg_date datetime default now(),
+//	read_cout int default 0,
+	
+	private long bno;
+	private String title;
+	private String writer;
+	private String content;
+	private String isDel;
+	private String regDate;
+	private int readCount;
+	
+}
---
+++ src/main/webapp/WEB-INF/views/layout/footer.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c "%>
+<!DOCTYPE html>
+	<div class="container-md">
+		2024 spring project...
+	</div>
+</body>
+</html>
